7c4fe0c43ac2f8cb532a696e68f41fed84308e6a
sidisaidkarim <sskarim85@gmail.com>
2025-09-23 21:23:21 +0200

    feat(component): network resources with terraform
    create vpc and subnet on aws with terraform, wip
    
    BREAKING CHANGE: create ressources

--- infrastructure/terraform/aws/main.tf
+++ infrastructure/terraform/aws/main.tf
@@ -8,9 +8,63 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3" # Paris, ou us-east-1 si tu préfères
+  region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "mon-bucket-terraform-demo-123456"
+resource "aws_vpc" "VPC-A" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "VPC-A"
+  }
+}
+
+resource "aws_subnet" "subnet-app" {
+  vpc_id = aws_vpc.VPC-A.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-3a"
+  tags = {
+    Name = "subnet-app"
+  }
+}
+
+resource "aws_subnet" "subnet-web" {
+  vpc_id = aws_vpc.VPC-A.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-3b"
+  tags = {
+    Name = "subnet-web"
+  }
+}
+
+resource "aws_subnet" "subnet-db" {
+  vpc_id = aws_vpc.VPC-A.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-3c"
+  tags = {
+    Name = "subnet-db"
+  }
+}
+
+resource "aws_internet_gateway" "vpc-a-igw" {
+  vpc_id = aws_vpc.VPC-A.id
+  tags = {
+    Name = "vpc-a-igw"
+  }
+}
+
+resource "aws_route_table" "vpc-a-rt-table" {
+  vpc_id = aws_vpc.VPC-A.id
+  tags = {
+    Name= "vpc-a-rt-tabl"
+  }
+}
+
+resource "aws_route_table_association" "vpc-a-rt-table-asso" {
+  subnet_id = aws_subnet.subnet-app.id
+  route_table_id = aws_route_table.vpc-a-rt-table.id
 }
